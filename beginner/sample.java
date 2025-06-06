89a24964b6ca506b6154ee2f80cc1f5d3323ba0b
ks24pon <pom123re0624@gmail.com>
2025-06-06 10:10:53 +0900

    https://recursionist.io/dashboard/problems/383

---
+++ beginner/sample.java
@@ -0,0 +1,7 @@
+class MyClass{
+    public static void main(String[] args){
+        int onlinePaidUser = 40;
+        int onlineUser = onlinePaidUser + 50;
+        System.out.println(onlineUser);
+    }
+}
