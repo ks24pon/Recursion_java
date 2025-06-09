d67fd8856b15d99f7dd006b90d87e955887199e0
ks24pon <pom123re0624@gmail.com>
2025-06-09 10:03:12 +0900

    https://recursionist.io/dashboard/course/1/lesson/53

---
+++ beginner/sample7.java
@@ -0,0 +1,10 @@
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+
+class Solution{
+    public static double metersToMiles(int meters){
+        return meters * 0.000621371;
+    }
+}
+
