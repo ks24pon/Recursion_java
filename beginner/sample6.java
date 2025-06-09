a6d1bfb2b753a3b9413fad1c9325c25949841aa1
ks24pon <pom123re0624@gmail.com>
2025-06-09 09:59:10 +0900

    https://recursionist.io/dashboard/course/1/lesson/52

---
+++ beginner/sample6.java
@@ -0,0 +1,10 @@
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+
+class Solution{
+    public static double circumferenceLength(int radius){
+        return 2 * radius * 3.14;
+    }
+}
+
