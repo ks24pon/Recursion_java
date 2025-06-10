6f6ea653ec823cd4e07568dfc78361a1a0758d7e
ks24pon <pom123re0624@gmail.com>
2025-06-10 09:32:54 +0900

    https://recursionist.io/dashboard/problems/39

---
+++ beginner/sample17.java
@@ -0,0 +1,12 @@
+class Solution{
+    public static String redirectionUrl(String language){
+        // 関数を完成させてください
+        String url = "www.example.org/";
+        if (language == "English") url += "en";
+        if (language == "Japanese") url += "ja";
+        if (language == "Spanish") url += "es";
+        if (language == "Russian") url += "ru";
+        if (language == "German");
+        return url;
+    }
+}
