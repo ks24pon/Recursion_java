e4df9b18e34a248c75e4ad6345990bd0b70ad50e
ks24pon <pom123re0624@gmail.com>
2025-06-24 15:13:45 +0900

    https://recursionist.io/dashboard/problems/355

---
+++ beginner/sample37.java
@@ -0,0 +1,21 @@
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+
+class Solution{
+    public static String calculateLocation(double latitude, double longitude){
+        // 関数を完成させてください
+        String northOrSouth;
+        String eastOrWest;
+
+        if(latitude == 0) northOrSouth = "equator";
+        else if(latitude < 0) northOrSouth = "south";
+        else northOrSouth = "north";
+
+        if(longitude == 0) eastOrWest = "prime meridian";
+        else if(longitude < 0) eastOrWest = "west";
+        else eastOrWest = "east";
+
+        return northOrSouth + "/" + eastOrWest;
+    }
+}
