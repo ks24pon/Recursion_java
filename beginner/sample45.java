fc5aa88ea997ff2051288908ab642a6333889839
ks24pon <pom123re0624@gmail.com>
2025-06-25 10:59:49 +0900

    https://recursionist.io/dashboard/problems/361

---
+++ beginner/sample45.java
@@ -0,0 +1,9 @@
+class Solution{
+    public static long factorial(int n){
+        // 関数を完成させてください
+        if(n <= 0){
+            return 1;
+        }
+        return factorial(n-1) * n;
+    }
+}
