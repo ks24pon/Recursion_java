8ae97c14316bc012066bc6f8a1c5717b7800f43d
ks24pon <pom123re0624@gmail.com>
2025-06-25 11:06:07 +0900

    https://recursionist.io/dashboard/problems/362

---
+++ beginner/sample46.java
@@ -0,0 +1,9 @@
+class Solution{
+    public static int squareSummation(int n){
+        // 関数を完成させてください
+        if(n <= 0){
+            return 0;
+        }
+        return squareSummation(n-1) + n * n;
+    }
+}
