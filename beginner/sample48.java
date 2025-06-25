6d1bb4f3ad847a36fa870d24b5398a0d7cd70cbf
ks24pon <pom123re0624@gmail.com>
2025-06-25 11:29:23 +0900

    https://recursionist.io/dashboard/course/2/lesson/167

---
+++ beginner/sample48.java
@@ -0,0 +1,9 @@
+class Solution{
+    public static int numberOfDots(int x){
+        // 関数を完成させてください
+        if(x == 1){
+            return 1;
+        }
+        return numberOfDots(x-1) + x;
+    }
+}
